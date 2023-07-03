1b5d94e5f22511b4314d769cf23bc66cc1bd9b94
TCN CI <ci@tcnp3.com>
2023-07-03 20:18:00 +0000

    update tcnapi-connect-es > Merge branch '76-add-omni-bot-protos-to-flows' into 'master'
    Resolve "Add omni bot protos to flows"
    
    Closes #76
    
    See merge request tcnpublic/api!82

--- src/api/commons/workflows/nodes_pb.js
+++ src/api/commons/workflows/nodes_pb.js
@@ -6,6 +6,7 @@
 import { proto3 } from "@bufbuild/protobuf";
 import { NodeChatbot, NodeComparator, NodeConsoleInput, NodePrint, NodeRandom, NodeStoreInput } from "./example_pb.js";
 import { OmniNodeError, OmniNodePrompt, OmniNodeSetSkill, OmniNodeToAgent } from "./omni_pb.js";
+import { OmniBotNodeTestEnd, OmniBotNodeTestStart, OmniBotNodeTestStep } from "./omni_bot_pb.js";
 
 /**
  * A node is a single step in a flow
@@ -30,6 +31,9 @@ export const NodeDefinition = proto3.makeMessageType(
     { no: 202, name: "omni_set_skill", kind: "message", T: OmniNodeSetSkill, oneof: "definition" },
     { no: 203, name: "omni_to_agent", kind: "message", T: OmniNodeToAgent, oneof: "definition" },
     { no: 204, name: "omni_error", kind: "message", T: OmniNodeError, oneof: "definition" },
+    { no: 301, name: "omni_bot_test_start", kind: "message", T: OmniBotNodeTestStart, oneof: "definition" },
+    { no: 302, name: "omni_bot_test_step", kind: "message", T: OmniBotNodeTestStep, oneof: "definition" },
+    { no: 303, name: "omni_bot_test_end", kind: "message", T: OmniBotNodeTestEnd, oneof: "definition" },
   ],
 );
 
