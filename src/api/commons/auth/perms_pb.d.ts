0762ec8306bfea3fb39051224555aa878f9e0fa7
TCN CI <ci@tcnp3.com>
2023-08-04 19:35:30 +0000

    update tcnapi-connect-es > Merge branch '117-add-omni-boss-perms-for-portals' into 'master'
    Resolve "add omni boss perms for portals"
    
    Closes #117
    
    See merge request tcnpublic/api!128

--- src/api/commons/auth/perms_pb.d.ts
+++ src/api/commons/auth/perms_pb.d.ts
@@ -395,6 +395,11 @@ export declare enum Permission {
   OMNI_BOSS = 1200,
 
   /**
+   * @generated from enum value: PERMISSION_OMNI_PORTALS_VIEW = 1210;
+   */
+  OMNI_PORTALS_VIEW = 1210,
+
+  /**
    * Enables access to viewing the integrations app.
    *
    * @generated from enum value: PERMISSION_INTEGRATIONS_VIEW = 1300;
