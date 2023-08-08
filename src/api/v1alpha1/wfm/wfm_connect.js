93623dd72ca77556545e72a260e7caac3081e89c
TCN CI <ci@tcnp3.com>
2023-08-08 20:59:03 +0000

    update tcnapi-connect-es > tweak

--- src/api/v1alpha1/wfm/wfm_connect.js
+++ src/api/v1alpha1/wfm/wfm_connect.js
@@ -1402,8 +1402,8 @@ export const WFM = {
      * Gets the inherited, own, and resulting bitmaps for the open times patterns of @node_to_check for @schedule_scenario_sid and the org sending the request.
      * The @schedule_scenario_sid must match the scenario of the @node_to_check.
      * If @bitmap_type is COMPLETE, the bitmaps will be generated using all relevant pattern data.
-     * If @bitmap_type is ONLY_WEEKMAPS, the bitmaps will be generated only using the weekmap data from the open times patterns.
-     * If @bitmap_type is ONLY_CALENDAR_ITEMS, the bitmaps will be generated only using the calendar item data from the open times patterns.
+     * If @bitmap_type is ONLY_WEEKMAPS, the bitmaps will be generated using only the weekmap data from the open times patterns.
+     * If @bitmap_type is ONLY_CALENDAR_ITEMS, the bitmaps will be generated using only the calendar item data from the open times patterns.
      * The bitmaps will be generated for the span of @datetime_range.
      * Required permissions:
      *   NONE
@@ -1483,8 +1483,8 @@ export const WFM = {
      * If an availability bitmap is requested for an agent group, the bitmaps for all of it's member agents will be returned instead.
      * The bitmaps will be generated for the span of @datetime_range.
      * If @bitmap_type is COMPLETE, the bitmaps will be generated using all relevant pattern data.
-     * If @bitmap_type is ONLY_WEEKMAPS, the bitmaps will be generated only using the weekmap data from the availability patterns.
-     * If @bitmap_type is ONLY_CALENDAR_ITEMS, the bitmaps will be generated only using the calendar item data from the availability patterns.
+     * If @bitmap_type is ONLY_WEEKMAPS, the bitmaps will be generated using only the weekmap data from the availability patterns.
+     * If @bitmap_type is ONLY_CALENDAR_ITEMS, the bitmaps will be generated using only the calendar item data from the availability patterns.
      * Required permissions:
      *   NONE
      * Errors:
