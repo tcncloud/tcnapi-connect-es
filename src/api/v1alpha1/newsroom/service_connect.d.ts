a5edacab80a725e46b3f1ee4d0d5d5b633e443fb
TCN CI <ci@tcnp3.com>
2023-09-15 19:26:43 +0000

    update tcnapi-connect-es > Merge branch '181-add-wfm-agent-portal-feature' into 'master'
    Resolve "add wfm agent portal feature"
    
    Closes #181
    
    See merge request tcnpublic/api!197

--- src/api/v1alpha1/newsroom/service_connect.d.ts
+++ src/api/v1alpha1/newsroom/service_connect.d.ts
@@ -3,7 +3,7 @@
 /* eslint-disable */
 // @ts-nocheck
 
-import { CreateNewsArticleRequest, CreateNewsArticleResponse, CreatePublishedArticleRequest, CreatePublishedArticleResponse, GetNewsArticleByIdRequest, GetNewsArticleByIdResponse, GetPublishedArticleByIdRequest, GetPublishedArticleByIdResponse, ListNewsArticlesRequest, ListNewsArticlesResponse, ListPublishedArticlesRequest, ListPublishedArticlesResponse, UpdateNewsArticleRequest, UpdateNewsArticleResponse } from "./entities_pb.js";
+import { CreateNewsArticleRequest, CreateNewsArticleResponse, CreatePublishedArticleRequest, CreatePublishedArticleResponse, GetNewsArticleByIdRequest, GetNewsArticleByIdResponse, GetNewsForUserRequest, GetNewsForUserResponse, GetPublishedArticleByIdRequest, GetPublishedArticleByIdResponse, ListNewsArticlesRequest, ListNewsArticlesResponse, ListPublishedArticlesRequest, ListPublishedArticlesResponse, UpdateNewsArticleRequest, UpdateNewsArticleResponse, UserActivityRequest, UserActivityResponse } from "./entities_pb.js";
 import { MethodKind } from "@bufbuild/protobuf";
 
 /**
@@ -91,6 +91,28 @@ export declare const NewsroomAPI: {
       readonly name: "GetPublishedArticleById",
       readonly I: typeof GetPublishedArticleByIdRequest,
       readonly O: typeof GetPublishedArticleByIdResponse,
+      readonly kind: MethodKind.Unary,
+    },
+    /**
+     * user activity updates
+     *
+     * @generated from rpc api.v1alpha1.newsroom.NewsroomAPI.UserActivity
+     */
+    readonly userActivity: {
+      readonly name: "UserActivity",
+      readonly I: typeof UserActivityRequest,
+      readonly O: typeof UserActivityResponse,
+      readonly kind: MethodKind.Unary,
+    },
+    /**
+     * fetch the unseen articles for the user
+     *
+     * @generated from rpc api.v1alpha1.newsroom.NewsroomAPI.GetNewsForUser
+     */
+    readonly getNewsForUser: {
+      readonly name: "GetNewsForUser",
+      readonly I: typeof GetNewsForUserRequest,
+      readonly O: typeof GetNewsForUserResponse,
       readonly kind: MethodKind.Unary,
     },
   }
