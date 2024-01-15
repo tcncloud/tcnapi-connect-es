c8c9110db885252cfee26f0d7803f6dfd97642a7
TCN CI <ci@tcnp3.com>
2024-01-15 18:47:25 +0000

    update tcnapi-connect-es > Merge branch '319-add-huntgroupagenttrigger-public-rpcs' into 'master'
    Resolve "Add HuntGroupAgentTrigger public rpcs"
    
    Closes #319
    
    See merge request tcnpublic/api!354

--- src/api/commons/omnichannel_pb.js
+++ src/api/commons/omnichannel_pb.js
@@ -39,7 +39,6 @@ export const SmsNumberProvider = proto3.makeEnum(
     {no: 4, name: "APEIRON_PROVIDER"},
     {no: 5, name: "AUSBURST_SMS_PROVIDER"},
     {no: 6, name: "MEDIASAT_SMS_PROVIDER"},
-    {no: 7, name: "TEXTLOCAL_SMS_PROVIDER"},
   ],
 );
 
