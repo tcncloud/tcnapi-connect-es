ae64660fd766eaefbe93e4240606f47ad9ace6b9
TCN CI <ci@tcnp3.com>
2024-04-23 20:03:27 +0000

    update tcnapi-connect-es > Merge branch '469-wfo-ca-add-filter-protos-to-v2' into 'master'
    Resolve "WFO-CA add filter protos to v2"
    
    Closes #469
    
    See merge request tcnpublic/api!495

--- src/wfo/vanalytics/v2/service_connect.d.ts
+++ src/wfo/vanalytics/v2/service_connect.d.ts
@@ -5,6 +5,9 @@
 
 import { SearchTranscriptsRequest, SearchTranscriptsResponse } from "./transcript_pb.js";
 import { MethodKind } from "@bufbuild/protobuf";
+import { CreateFilterRequest, DeleteFilterRequest, DeleteFilterResponse, Filter, GetFilterRequest, ListFiltersRequest, ListFiltersResponse, UpdateFilterRequest } from "./filter_pb.js";
+import { ListFlagTranscriptFiltersRequest, ListFlagTranscriptFiltersResponse } from "./flag_transcript_filter_pb.js";
+import { ListFlagFiltersRequest, ListFlagFiltersResponse } from "./flag_filter_pb.js";
 
 /**
  * @generated from service wfo.vanalytics.v2.Vanalytics
@@ -23,6 +26,84 @@ export declare const Vanalytics: {
       readonly name: "SearchTranscripts",
       readonly I: typeof SearchTranscriptsRequest,
       readonly O: typeof SearchTranscriptsResponse,
+      readonly kind: MethodKind.Unary,
+    },
+    /**
+     * CreateFilter creates a new filter. The filter contains a transcript query
+     * to filter transcripts.
+     *
+     * @generated from rpc wfo.vanalytics.v2.Vanalytics.CreateFilter
+     */
+    readonly createFilter: {
+      readonly name: "CreateFilter",
+      readonly I: typeof CreateFilterRequest,
+      readonly O: typeof Filter,
+      readonly kind: MethodKind.Unary,
+    },
+    /**
+     * ListFilters lists filters.
+     *
+     * @generated from rpc wfo.vanalytics.v2.Vanalytics.ListFilters
+     */
+    readonly listFilters: {
+      readonly name: "ListFilters",
+      readonly I: typeof ListFiltersRequest,
+      readonly O: typeof ListFiltersResponse,
+      readonly kind: MethodKind.Unary,
+    },
+    /**
+     * UpdateFilter updates a filter transcript query and/or name.
+     *
+     * @generated from rpc wfo.vanalytics.v2.Vanalytics.UpdateFilter
+     */
+    readonly updateFilter: {
+      readonly name: "UpdateFilter",
+      readonly I: typeof UpdateFilterRequest,
+      readonly O: typeof Filter,
+      readonly kind: MethodKind.Unary,
+    },
+    /**
+     * DeleteFilter deletes filter given a filter_sid.
+     *
+     * @generated from rpc wfo.vanalytics.v2.Vanalytics.DeleteFilter
+     */
+    readonly deleteFilter: {
+      readonly name: "DeleteFilter",
+      readonly I: typeof DeleteFilterRequest,
+      readonly O: typeof DeleteFilterResponse,
+      readonly kind: MethodKind.Unary,
+    },
+    /**
+     * GetFilter gets filter given a filter_sid or name.
+     *
+     * @generated from rpc wfo.vanalytics.v2.Vanalytics.GetFilter
+     */
+    readonly getFilter: {
+      readonly name: "GetFilter",
+      readonly I: typeof GetFilterRequest,
+      readonly O: typeof Filter,
+      readonly kind: MethodKind.Unary,
+    },
+    /**
+     * ListFlagTranscriptFilters lists flag transcript filters in an organization.
+     *
+     * @generated from rpc wfo.vanalytics.v2.Vanalytics.ListFlagTranscriptFilters
+     */
+    readonly listFlagTranscriptFilters: {
+      readonly name: "ListFlagTranscriptFilters",
+      readonly I: typeof ListFlagTranscriptFiltersRequest,
+      readonly O: typeof ListFlagTranscriptFiltersResponse,
+      readonly kind: MethodKind.Unary,
+    },
+    /**
+     * ListFlagFilters lists flag filter associations.
+     *
+     * @generated from rpc wfo.vanalytics.v2.Vanalytics.ListFlagFilters
+     */
+    readonly listFlagFilters: {
+      readonly name: "ListFlagFilters",
+      readonly I: typeof ListFlagFiltersRequest,
+      readonly O: typeof ListFlagFiltersResponse,
       readonly kind: MethodKind.Unary,
     },
   }
