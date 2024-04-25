7131c16e71dce854a3ba3ec921b5b8ea7fa34fad
TCN CI <ci@tcnp3.com>
2024-04-25 23:07:35 +0000

    update tcnapi-connect-es > format

--- src/api/commons/omnichannel_pb.js
+++ src/api/commons/omnichannel_pb.js
@@ -570,6 +570,7 @@ export const OmniMessage = /*@__PURE__*/ proto3.makeMessageType(
     { no: 19, name: "subject", kind: "message", T: StringValue },
     { no: 21, name: "sender_type", kind: "enum", T: proto3.getEnumType(OmniSenderType) },
     { no: 22, name: "status_message", kind: "message", T: StringValue },
+    { no: 23, name: "message_format", kind: "enum", T: proto3.getEnumType(MessageFormat) },
   ],
 );
 
