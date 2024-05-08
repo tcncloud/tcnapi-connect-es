fde36d1b608e79479940a01d616d9d940c35e651
TCN CI <ci@tcnp3.com>
2024-05-08 20:33:57 +0000

    update tcnapi-connect-es > feat(billing): add default rate definition id to create request

--- src/services/billing/v1alpha2/rates_pb.js
+++ src/services/billing/v1alpha2/rates_pb.js
@@ -56,6 +56,7 @@ export const CreateRateDefinitionRequest = /*@__PURE__*/ proto3.makeMessageType(
   () => [
     { no: 1, name: "rate_definition_id", kind: "scalar", T: 9 /* ScalarType.STRING */ },
     { no: 2, name: "rate_definition", kind: "message", T: RateDefinition },
+    { no: 3, name: "default_rate_definition_id", kind: "scalar", T: 9 /* ScalarType.STRING */ },
   ],
 );
 
